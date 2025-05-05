d21493ac9b1911af699014ce3e88952bfd65f02a
tyranotyrano <tyranotyrano@woowahan.com>
2025-05-05 16:06:06 +0900

    패키지 정리 및 readme 내용 추가

--- src/main/kotlin/com/tyranocraft/frontend/README.md
+++ src/main/kotlin/com/tyranocraft/frontend/README.md
@@ -2,7 +2,11 @@
 
 1. npm install sass
     - CSS를 더 편하게 쓸 수 있게 해주는 CSS 확장 언어
+    - scss 파일을 사용하기 위해 설치
+    - {name}.module.scss 형식으로 생성
 2. npm install @craco/craco
     - "@" 으로 scss 경로 단축을 위한 Webpack alias 설정
+    - component 가 있는 곳에 index.ts 생성하여 사용 가능
 3. npm install zustand
-    - 상태관리 라이브러이
+    - 상태관리 라이브러리
+    - [useAuthStore.ts](src/store/useAuthStore.ts) 참고
--- src/main/kotlin/com/tyranocraft/frontend/craco.config.js
+++ src/main/kotlin/com/tyranocraft/frontend/craco.config.js
@@ -1,3 +1,16 @@
+/*
+1. craco 사용을 위해 src(= 루트경로)와 동일 레벨에 추가해야함!
+2. package.json의 scripts에서 react-scripts 대신 craco를 사용하도록 변경해야함
+  - react-scripts start -> craco start
+  - react-scripts build -> craco build
+  - react-scripts test -> craco test
+3. tsconfig.json 에서 paths 설정을 추가해야함
+  - "baseUrl": "src",
+    "paths": {
+      "@/*": ["*"]
+    },
+*/
+
 const path = require("path");
 
 module.exports = {
